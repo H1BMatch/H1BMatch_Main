5157f49f02327b617cb852419ead84f64fa90687
acbikash13 <acbikash13@gmail.com>
2024-11-24 12:54:36 -0500

    Bikash

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import * as userService from "../services/userService";
+import containerClient from "../utils/connectToAzureBlobStorage";
+const multer = require("multer");
+
+
 import {
   ClerkExpressRequireAuth,
 } from "@clerk/clerk-sdk-node";
@@ -22,6 +26,50 @@ userRoutes.get(
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ message: "Error fetching user", error });
+    }
+  }
+);
+
+// user routes to post the profile picture for the user
+const storage = multer.memoryStorage();
+const upload = multer({ storage }).single("profilePicture");
+// file might not be recognized as a property of Request. So extend it to include the file as the multer request type.
+interface MulterRequest extends Request {
+  file: any;
+}
+userRoutes.post(
+  "/upload-profile-picture", upload,
+  async (req: Request, res: Response) => {
+    try {
+      const userId = req.auth?.userId;
+      if (!userId) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+      const documentFile = (req as MulterRequest).file;
+      if (!documentFile) {
+        return res.status(400).json({ message: "No file uploaded" });
+      }
+      const imageBuffer = documentFile.buffer;
+      const imageType = documentFile.mimetype;
+      // we might need to change how we name the image file. Can user ID but wont be a good idea security wise. The name for the image need to be unique.
+      const imageName = documentFile.originalname + Date.now();
+
+      //upload the imageBuffer to the azure blobStorage
+      await containerClient.uploadBlockBlob(imageName, imageBuffer, imageType);
+
+      const blobUrl = containerClient.getBlockBlobClient(imageName).url;
+      // update the user profile picture URL in the database
+      await updateUserProfilePictureUrl(userId, blobUrl);
+
+      res
+        .status(200)
+        .json({
+          message: "Profile picture uploaded successfully",
+          profileUrl: blobUrl,
+        });
+    } catch (error: any) {
+      console.error("Error uploading profile picture:", error);
+      res.status(500).json({ error: "Failed to upload profile picture" });
     }
   }
 );
---
+++ src/utils/connectToAzureBlobStorage.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
+
+/**
+ * Initializes and returns an Azure Blob Storage container client.
+ * @returns {ContainerClient} The container client object to interact with the container in Azure Blob Storage.
+ * @throws Will throw an error if connection string or container name is not provided.
+ */
+export function initializeContainerClient(): ContainerClient {
+  const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+  const CONTAINER_NAME = "userimages";
+
+  if (!AZURE_STORAGE_CONNECTION_STRING) {
+    throw new Error("Azure Storage Connection string not found");
+  }
+
+  if (!CONTAINER_NAME) {
+    throw new Error("Container name not found");
+  }
+
+  const blobServiceClient: BlobServiceClient = 
+    BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+  const containerClient: ContainerClient = 
+    blobServiceClient.getContainerClient(CONTAINER_NAME);
+
+  console.log("Azure Blob Storage initialized successfully.");
+  return containerClient;
+}
+
+// Usage:
+// const containerClient = initializeContainerClient();
+export default initializeContainerClient;
