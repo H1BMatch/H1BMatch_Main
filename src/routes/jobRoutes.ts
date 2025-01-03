83a20d3fc65f4d4760af084b8daebc4a2353fe1e
acbikash13 <acbikash13@gmail.com>
2025-01-03 18:38:55 -0500

    search feature

--- src/routes/jobRoutes.ts
+++ src/routes/jobRoutes.ts
@@ -18,25 +18,23 @@ jobRoutes.get(
       return res.status(403).json({ error: 'Unauthorized' });
     }
     try {
-      const filters ={}
-      // // Extract filters from query parameters
-      // const filters = {
-      //   title: req.query.title as string,
-      //   company: req.query.company as string,
-      //   city: req.query.city as string,
-      //   state: req.query.state as string,
-      //   country: req.query.country as string,
-      //   job_type: req.query.job_type as string,
-      //   is_remote: req.query.is_remote === 'true',
-      //   date_posted: req.query.date_posted as string, // days ago
-      //   min_salary: req.query.min_salary as string,
-      //   max_salary: req.query.max_salary as string,
-      //   job_level: req.query.job_level as string,
-      //   company_industry: req.query.company_industry as string,
-      //   is_sponsor: req.query.is_sponsor === 'true' || false,
-      // };
-      console.log('filters:', req.query);
-
+      // Extract filters from query parameters
+      const filters = {
+        title: req.query.title as string,
+        company: req.query.company as string,
+        city: req.query.city as string,
+        state: req.query.state as string,
+        country: req.query.country as string,
+        job_type: req.query.job_type as string,
+        is_remote: req.query.is_remote === 'true',
+        date_posted: req.query.date_posted as string, // days ago
+        min_salary: req.query.min_salary as string,
+        max_salary: req.query.max_salary as string,
+        job_level: req.query.job_level as string,
+        company_industry: req.query.company_industry as string,
+        is_sponsor: req.query.is_sponsor === 'true' || false,
+      };
+      console.log("Filters are ", filters);
       const jobs = await getJobsBySimilarity(userId, filters);
       res.status(200).json(jobs);
     } catch (error: any) {
